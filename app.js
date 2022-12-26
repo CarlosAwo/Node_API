4f4d72c18587d00fc6ea4903f97f4d2c83daf4b7
Carlos Awo <awocarlos55@gmail.com>
2022-12-26 12:18:05 +0100

    Demarrge du serveur Express, installation et configuration de nodemon

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello the world :smile_cat " );
+const express = require ("express")
+const app = express()
+
+const port = 8000
+const baseUrl = `http://localhost:${port}`
+
+app.get("/", (req,res)=>{
+    res.send("hello the world 8888 !!!")
+})
+app.listen(port     , ()=>{console.log( `Server started at ${baseUrl}`)})
