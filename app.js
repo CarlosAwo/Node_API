24b76d01d48e2d56316bce4e79f470988f92147f
Carlos Awo <awocarlos55@gmail.com>
2022-12-26 13:53:06 +0100

    Mise en place des routes de bases

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require ("express")
 const app = express()
 
+const pokemons = require("./data")
 const port = 8000
 const baseUrl = `http://localhost:${port}`
 
 app.get("/", (req,res)=>{
-    res.send("hello the world 8888 !!!")
+    res.send("Welcome Sir !!!")
 })
+
+app.get("/api/pokemons", (req,res)=>{
+    let msg =  "Aucun Pokemon n'a été trouvé"
+    if(pokemons.length !=0 ){msg =`Nous avons trouvé  ${pokemons.length} pokemons`    }
+    const result = {
+        msg:msg,
+        data:pokemons
+    }
+    res.json(result)
+})
+
+app.get("/api/pokemons/:id", (req,res)=>{
+    let id = req.params.id
+    let msg =  "Aucun Pokemon n'a été trouvé"
+    let pokemon = null
+    pokemons.forEach(
+        (child)=>{
+            if(child.id == id)
+            pokemon = child
+        }
+    )
+    if(pokemon != null){msg = `Nous avons trouvé le Pokemon ${id}`}
+    const result = {
+        msg:msg,
+        data:pokemon
+    }
+    res.json(result)
+})
+
 app.listen(port     , ()=>{console.log( `Server started at ${baseUrl}`)})
